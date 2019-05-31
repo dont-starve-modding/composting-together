8015dbebceb19a80e0e6e935f3eb5c13cd5c3ee8
Aehrraid <Aehrraid@users.noreply.github.com>
2019-05-31 11:12:00 +0200

    new configuration options

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ name = "Composting Together"
 description = "Adds a compost pile to the game. Make the best out of your spoiled food!"
 author = "s1m13"
 
-version = "0.1.0"
+version = "0.2.0"
 
 api_version = 10
 
@@ -23,5 +23,58 @@ server_filter_tags = { "composting", "compost", "structure", "manure", "producti
 
 configuration_options =
 {
+    {
+        name = "poop_amount",
+        label = "Poop amount",
+        options = {
+            { description = "Low", data = "low" },
+            { description = "Default", data = "default" },
+            { description = "High", data = "high" },
+        },
+        default = "default",
+    },
 
+    {
+        name = "compost_duration",
+        label = "Compost duration",
+        options = {
+            { description = "Realistic", data = "realistic" },
+            { description = "Default", data = "default" },
+            { description = "Efficient", data = "efficient" },
+        },
+        default = "default",
+    },
+
+    {
+        name = "cost",
+        label = "Cost",
+        options = {
+            { description = "Low", data = "low" },
+            { description = "Default", data = "default" },
+            { description = "High", data = "high" },
+        },
+        default = "default",
+    },
+
+    {
+        name = "fertile_soil_advantage",
+        label = "Fertile soil advantage",
+        options = {
+            { description = "Low", data = "low" },
+            { description = "Default", data = "default" },
+            { description = "High", data = "high" },
+        },
+        default = "default",
+    },
+
+    {
+        name = "spawn_fireflies",
+        label = "Attract Fireflies",
+        options = {
+            { description = "Always", data = "always" },
+            { description = "On", data = "on" },
+            { description = "Off", data = "off" },
+        },
+        default = "on",
+    },
 }
